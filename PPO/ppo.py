67260689814676f3d0ae29ddf185a2cdaea5fef7
pJoengw <iwbyyyy@gmail.com>
2020-12-18 14:35:11 +0800

    fuck

--- PPO/ppo.py
+++ PPO/ppo.py
@@ -4,20 +4,24 @@ import numpy as np
 import tensorflow as tf
 from tensorflow import keras
 from collections import deque
+import os
 
-from tensorflow._api.v2.data import Dataset
-from tensorflow.python.keras.backend_config import epsilon
+
+# Visualize 
+root_logdir = os.path.join(os.curdir, 'my_logs')
+def get_run_logdir():
+    import time
+    run_id = time.strftime("run_%Y_%m_%d-%H_%M_%S")
+    return os.path.join(root_logdir, run_id)
 
 class get_config():
-    max_len = 1e4
-    iterations = 1e4
-    n_max_steps = 200
+    max_len = 1000
+    iterations = 10
+    n_max_steps = 600
     n_fixed_steps = 2048
-    discount_factor = 0.95
-    ENV_NAME = 'Reacher-v2'
-    batch_size = 64
-    discount_factor = 0.95
-    lamda = 0.9
+    discount_factor = 0.99
+    ENV_NAME = 'Pendulum-v0'
+    lamda = 0.95
     epsilon = 0.2
 
 
@@ -30,13 +34,13 @@ class ActorCritic(keras.Model):
             bound:          放大倍数
 
         '''
-
+        super().__init__()
         self.state_space = self._to_list(state_space)
         self.action_space = self._to_list(action_space)
         # self.bound = self._to_array(bound)
         self.bound = bound
 
-        if len(self.action_space != 1):
+        if len(self.action_space) != 1:
             raise ValueError('动作空间的维度是{}，模型只接受一维动作序列'.format(self.action_space))
         
         # if self.action_space != self.bound.shape[0]:
@@ -45,9 +49,9 @@ class ActorCritic(keras.Model):
         # if len(self.bound.shape) == 1:
         #     self.bound = self.bound.repeat(2).reshape(self.bound.shape[0], -1)
         # action
-        ac_input = keras.layers.Flatten(input_shape=self.state_space)
-        ac_layer1 = keras.layers.Dense(64)(ac_input)
-        ac_layer2 = keras.layers.Dense(64)(ac_layer1)
+        ac_input = keras.layers.Input(shape=self.state_space)
+        ac_layer1 = keras.layers.Dense(64, activation='tanh')(ac_input)
+        ac_layer2 = keras.layers.Dense(64, activation='tanh')(ac_layer1)
         ac_layer3 = keras.layers.Dense(
             self.action_space[0], activation='tanh')(ac_layer2)
         # tanh函数取值范围是[-1,1]，因此，需要放大bound倍，以适应action_space 的不同取值。
@@ -56,19 +60,19 @@ class ActorCritic(keras.Model):
             self.action_space[0], activation='softplus')(ac_layer3)
 
         # critic
-        cr_input = keras.layers.Flatten(input_shape=self.state_space)
-        cr_layer1 = keras.layers.Dense(64)(cr_input)
-        cr_layer2 = keras.layers.Dense(64)(cr_layer1)
+        cr_input = keras.layers.Input(shape=self.state_space)
+        cr_layer1 = keras.layers.Dense(64, activation='tanh')(cr_input)
+        cr_layer2 = keras.layers.Dense(64, activation='tanh')(cr_layer1)
         cr_outputs = keras.layers.Dense(1)(cr_layer2)
 
         self.actor = keras.Model(
-            inputs=ac_input, outputs=[ac_mean, ac_logstd])
-        self.critic = keras.Model(inputs=cr_input, outputs=[cr_outputs])
+            inputs=[ac_input], outputs=[ac_mean, ac_logstd])
+        self.critic = keras.Model(inputs=[cr_input], outputs=[cr_outputs])
 
     def call(self, state):
         ac_mean, ac_logstd = self.actor(state)
         value = self.critic(state)
-        return ac_mean, ac_logstd, value
+        return tf.squeeze(ac_mean), tf.squeeze(ac_logstd), tf.squeeze(value)
 
     @staticmethod
     def _normal_logproba(x, mean, logstd, std=None):
@@ -83,13 +87,13 @@ class ActorCritic(keras.Model):
 
     def _choose_action(self, mean, logstd, return_old_log=True):
         std = tf.math.exp(logstd)
-        action = tf.random.normal([self.action_space[0]], mean, std, tf.float32)
+        action = tf.random.normal(self.action_space, mean, std, tf.float32)
         log_probs = ActorCritic._normal_logproba(action, mean, logstd, std)
         cliped_action = tf.clip_by_value(action, -1 * self.bound,  1 * self.bound)
         if return_old_log:
-            return cliped_action, log_probs
+            return tf.squeeze(cliped_action), log_probs
         else:
-            return cliped_action
+            return tf.squeeze(cliped_action)
 
     def _to_list(self, x):
         """Normalizes a list/tensor into a list.
@@ -107,67 +111,71 @@ class ActorCritic(keras.Model):
             return x
         return [i.__int__() for i in x]
 
-    def _to_array(self, x):
-        return np.array(x)
 
+def ppo(config, env):
 
-
-def ppo():
-
-    config = get_config()
-    batch_size = config.batch_size
+    # Hyper Params Setting
     buffer = deque(maxlen=config.max_len)
     interations = config.iterations
     n_max_steps = config.n_max_steps
     n_fixed_steps = config.n_fixed_steps
-    env = gym.make(config.ENV_NAME)
+
     discount_factor = config.discount_factor
     lamda = config.lamda
     epsilon = config.epsilon
 
+    # Define Network
     out_net = ActorCritic(env.observation_space.shape,env.action_space.shape, 1)
     new_net = ActorCritic(env.observation_space.shape,env.action_space.shape, 1)
     optimizer = keras.optimizers.Adam(learning_rate=1e-3)
 
+    # visual 
+    loss_logdir = get_run_logdir()
+    writer = tf.summary.create_file_writer(loss_logdir)
 
     for interation in range(interations):
-        # all_values = []
-        # all_rewards = []
-        global_steps = 0
+
+        print("epoch: {}".format(interation))
         out_net.set_weights(new_net.get_weights())
 
+        global_steps = 0
         for t in range(n_fixed_steps):
+            buffer.clear()
             state = env.reset()
 
+            state = np.expand_dims(state, axis=0).astype('float32')
+
             rewards = 0
-            step = 0
+            # 收集数据
             for step in range(n_max_steps):
                 ac_mean, ac_logstd, value = out_net(state)
                 action, log_prob = out_net._choose_action(ac_mean, ac_logstd)
                 action = action.numpy()
                 log_prob = log_prob.numpy()
-
-                next_state, reward, done, info = env.step(action)
+                next_state, reward, done, info = env.step([action])
+                next_state = next_state.reshape(1,-1)
+                # env.render()
                 mask = 0 if done else 1
                 rewards += reward
+                # tf.summary.scalar('reward_{}'.format(interation),float(rewards), step=global_steps)
 
                 buffer.append(
                     [state, value, action, log_prob, mask, reward])
 
+                state = next_state
+                global_steps += 1
                 if done:
                     break
-                state = next_state
-
-            t = step + 1
-
-            # Update Parameters
-            # Get Data
-
+            
+            # print("t = {}, global_steps = {}".format(t, global_steps))
             all_states, all_values, all_actions, all_old_probs, all_masks, all_rewards = [
                 [buffer[i][var_index] for i in range(len(buffer))]
                 for var_index in range(6)
             ]
+            all_states = np.squeeze(all_states)
+
             all_advantages = np.zeros(shape=len(all_values))
+
             # tf.GradientTape Context:
             with tf.GradientTape(persistent=True) as tape:
 
@@ -185,18 +193,22 @@ def ppo():
                     pre_reward = all_rewards[i]
                     pre_value = all_values[i]
                     pre_advantage = all_advantages[i]
-
                 ac_mean, ac_logstd, cr_values = new_net(all_states)
                 ac_logproba = ActorCritic._normal_logproba(all_actions, ac_mean, ac_logstd)
 
                 rio = tf.math.exp(ac_logproba / all_old_probs)
                 cliped_rio = tf.clip_by_value(rio, 1-epsilon, 1+epsilon)
-                loss = tf.math.minimum(rio * all_advantages, cliped_rio * all_advantages)
-                ac_loss = -tf.reduce_mean(loss)
+                a_loss = tf.math.minimum(rio * all_advantages, cliped_rio * all_advantages)
+                ac_loss = -tf.reduce_mean(a_loss)
+                
+                c_loss = keras.losses.mean_squared_error(cr_values, all_rewards)
+                cr_loss = tf.reduce_mean(c_loss)
 
-                loss = keras.losses.mean_squared_error(cr_values, all_rewards)
-                cr_loss = tf.reduce_mean(loss)
+                print("interation = {} epoch: {} , ac_loss = {}, cr_loss = {}".format(interation, t, ac_loss, cr_loss))
+
+            # Update Gradient
             new_net.critic.trainable = False
+            new_net.actor.trainable=True
             ac_gradients = tape.gradient(ac_loss, new_net.trainable_variables)
             optimizer.apply_gradients(zip(ac_gradients, new_net.trainable_variables))
             new_net.actor.trainable = False
@@ -204,5 +216,23 @@ def ppo():
             cr_gradients = tape.gradient(cr_loss, new_net.trainable_variables)
             optimizer.apply_gradients(zip(cr_gradients, new_net.trainable_variables))
             del tape
+            with writer.as_default():
+                tf.summary.scalar('policy_loss_{}'.format(t), ac_loss, t)
+                tf.summary.scalar('value_loss_{}'.format(t), cr_loss, t)
+
+            if global_steps > n_fixed_steps:
+                break
+
 
         
+if __name__=='__main__':
+
+    config = get_config()
+
+    np.random.seed(42)
+    tf.random.set_seed(42)
+    env = gym.make(config.ENV_NAME)
+    env.seed(42)
+
+    ppo(config, env)
+    
